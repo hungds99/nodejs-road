e8a7480aa0611392ad340249aada9a2e25b72b48
hungds99 <34293141+hungds99@users.noreply.github.com>
2023-09-27 10:32:20 +0700

    :zap: update transform function readable stream

--- stream/read-stream.js
+++ stream/read-stream.js
@@ -1,38 +1,40 @@
-const { ReadableStream } = require('node:stream/web');
+import { ReadableStream } from 'node:stream/web';
+import { setInterval as every } from 'node:timers/promises';
+import { performance } from 'node:perf_hooks';
 
-const { setInterval: every } = require('node:timers/promises');
+// ReadableStream performance timer interval
+const readStreamTimerInterval = async () => {
+  const SECOND = 1000;
 
-const { performance } = require('node:perf_hooks');
-
-const SECOND = 1000;
-
-// Create a readable stream
-const readableStream = new ReadableStream({
-  // A function that is called when the stream is started
-  async start(controller) {
-    for await (const _ of every(SECOND)) {
-      controller.enqueue(`Start: ${performance.now()}`);
+  // Create a readable stream
+  const readableStream = new ReadableStream({
+    // A function that is called when the stream is started
+    async start(controller) {
+      for await (const _ of every(SECOND)) {
+        controller.enqueue(`Start: ${performance.now()}`);
+      }
     }
-  }
-});
+  });
 
-// Transform data in the stream
-const transform = new TransformStream({
-  async transform(chunk, controller) {
-    controller.enqueue(chunk.toUpperCase());
-  }
-});
+  // Transform data in the stream
+  const transform = new TransformStream({
+    async transform(chunk, controller) {
+      controller.enqueue(chunk.toUpperCase());
+    }
+  });
 
-// Pipe the stream to the transform
-const transformedStream = readableStream.pipeThrough(transform);
+  // Pipe the stream to the transform
+  const transformedStream = readableStream.pipeThrough(transform);
 
-// Can not use `readableStream` after piping
-// console.log(readableStream.locked);
-// const reader = readableStream.getReader();
-// console.log(reader.read());
+  // Can not use `readableStream` after piping
+  // console.log(readableStream.locked);
+  // const reader = readableStream.getReader();
+  // console.log(reader.read());
 
-(async () => {
+  // Read the transformed stream
   for await (const chunk of transformedStream) {
     console.log(chunk);
   }
-})();
+};
+
+readStreamTimerInterval();
