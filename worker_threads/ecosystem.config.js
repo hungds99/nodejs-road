0f9aca36726797b5dc3a1c95512f1c02e1417af2
hungds99 <34293141+hungds99@users.noreply.github.com>
2023-09-28 09:36:26 +0700

    :tada: add pm2 monitor

---
+++ worker_threads/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'worker_threads',
+      script: 'index.js',
+      watch: '.',
+      ignore_watch: 'node_modules',
+      instances: 'max',
+      exec_mode: 'cluster',
+      out_file: './scripts.txt',
+      error_file: './error.txt'
+    }
+  ]
+};
--- worker_threads/index.js
+++ worker_threads/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { Worker, isMainThread } = require('worker_threads');
+import express from 'express';
+import { Worker, isMainThread } from 'worker_threads';
 const app = express();
 const port = 3000;
 
@@ -7,10 +7,12 @@ app.get('/', (req, res) => {
   console.log('Hello from the main thread!');
 
   res.send(`
-  <div>
-    <a href="/non-blocking">Non-blocking</a>
-    <a href="/blocking">Blocking</a>
-  </div>`);
+    <div>
+      <a href="/non-blocking">Non-blocking</a>
+      <a href="/blocking">Blocking</a>
+      <a href="/worker-threads">Worker threads</a>
+    </div>
+  `);
 });
 
 app.get('/non-blocking', (req, res) => {
@@ -20,11 +22,20 @@ app.get('/non-blocking', (req, res) => {
 
 app.get('/blocking', (req, res) => {
   console.log('Routing to blocking route');
+  let count = 0;
+  for (let i = 0; i < 10000000000; i++) {
+    count++;
+  }
+  res.send(`Hello from blocking route! ${count + 1}`);
+});
+
+app.get('/worker-threads', (req, res) => {
+  console.log('Routing to worker threads route');
   const worker = new Worker('./workers.js', { workerData: 10 });
   console.log('Is main thread?', isMainThread);
   worker.postMessage('count');
   worker.on('message', (count) => {
-    res.send(`Hello from blocking route! ${count}`);
+    res.send(`Hello from worker threads route! ${count}`);
   });
 });
 
