5f16e7527c6b7822310a5bba8736a73e9fdec122
hungds99 <34293141+hungds99@users.noreply.github.com>
2023-10-03 09:12:07 +0700

    :seedling: add event loop example

---
+++ worker_threads/event-loop.js
@@ -0,0 +1,12 @@
+// Demo 1
+console.log('first');
+for (let i = 0; i < 1000000000000; i++) {}
+console.log('second');
+
+// Demo 2
+// import fs from 'fs';
+// console.log('first');
+// fs.readFile('./big-data.txt', (err, data) => {
+//   console.log('second');
+// });
+// console.log('third');
